3e215b179004154fd50f2d31efb837bc1bd989bc
MrNarman <godricjnarman21@gmail.com>
2024-04-16 13:34:30 +0300

    Added user input and evaluation of the input with the random number

--- src/App.java
+++ src/App.java
@@ -2,19 +2,54 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        int randomNO, trialMIN, trialMAX, userGuess;
+        int randomNO;
+        int trialMIN;
+        int trialMAX;
+        int userGuess;
+
         String userName;
 
         //Generates random number 
-        randomNO = (int) Math.random()* 10 + 1;
+        randomNO = (int) (Math.random()* 10) + 1;
 
+        //Get user input
         Scanner userInput = new Scanner(System.in);
 
         System.out.print("Enter your Name: ");
         userName = userInput.nextLine();
 
-        System.out.print("Welcome "+ userName +", Enter your Guess: " );
+        trialMIN = 1;
+        trialMAX = 3;
+
+        System.out.println("========================================================");
+        System.out.println("                     Welcome                            ");
+        System.out.println("=================    "+userName.toUpperCase()+"    ===================");
+        System.out.println("========================================================");
+
+        System.out.print("You have "+ trialMAX +" trials to guess a number."+" Enter your Guess: " );
         userGuess = userInput.nextInt();
+
+        //Evaluate user input
+        do{
+            if (userGuess != randomNO) {
+                System.out.print(userGuess + " is not the number. Try again: ");
+                userGuess = userInput.nextInt();                
+            } else{
+                System.out.print("Hurray! "+userGuess+ " is the number.");
+                userInput.close();
+                break;
+            }
+
+            trialMIN++;
+            if ((trialMIN == trialMAX) && userGuess != randomNO) {
+                System.out.println("You Failed to guess the correct number.");             
+                
+            }
+            
+        }while(trialMIN < trialMAX);            
+
+        System.out.println("No more trials allowed!");
 
     }
 }
+
